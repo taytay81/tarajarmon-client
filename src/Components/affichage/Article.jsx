7cc48012099e58e00b4a0889ea7dbde472b33c1c
unknown <tayssire@hotmail.com>
2020-10-20 15:38:42 +0200

    first version loading

--- src/Components/affichage/Article.jsx
+++ src/Components/affichage/Article.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import "../../Styles/Articles.css";
+
 export default class Article extends Component {
+  state = {
+    loadingClassNameimg: " unArticle_img_hidden",
+    loadingClassNameLogo: "unArticle_loading_logo_visible",
+  };
+  changeLoadingImg() {
+    console.log("loaded");
+    this.setState({ loadingClassNameimg: "unArticle_img_visible" });
+    this.setState({ loadingClassNameLogo: "unArticle_loading_logo_hidden" });
+  }
+
   render() {
+    console.log("test", this.props.articleValue.image[0]);
     return (
       <div className="unArticle">
-        <div className="unArticle_img">
-          <img src={this.props.articleValue.image[0]} alt="" />
+        <div className={this.state.loadingClassNameimg}>
+          <img
+            src={this.props.articleValue.image[0]}
+            alt=""
+            onLoad={() => this.changeLoadingImg()}
+          />
+        </div>
+        <div className={this.state.loadingClassNameLogo}>
+          <img src="../icone.jpg" alt="" />
         </div>
         <h2 className="unArticle_titre">{this.props.articleValue.titre}</h2>
         <div className="unArticle_prix">{this.props.articleValue.prix} dt </div>
--- src/Components/affichage/Article_nav.jsx
+++ src/Components/affichage/Article_nav.jsx
@@ -3,9 +3,9 @@ import "../../Styles/Articles.css";
 export default function Article_nav() {
   return (
     <div className="article_nav_container">
-      <div> ARTICLES</div>
+      {/*<div> ARTICLES</div>
       <div>Filtrer PAR</div>
-      <div>PAGES</div>
+      <div>PAGES</div>*/}
     </div>
   );
 }
